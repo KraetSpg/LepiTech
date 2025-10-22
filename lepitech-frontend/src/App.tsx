570c2e84f2425f7e999dbb6255c9d2e4fdc37028
Ilvus32 <dei220264@spengergasse.at>
2025-10-22 18:59:28 +0200

    install components for nav bar

--- lepitech-frontend/src/App.tsx
+++ lepitech-frontend/src/App.tsx
@@ -1,35 +1,36 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+//import reactLogo from './assets/react.svg'
+//import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
+/*<div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      </div>*/
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+  <header className="flex items-center justify-between bg-gray-100 p-4">
+    <div className="flex items-center gap-2">
+      <span className="font-bold text-lg">Logo</span>
+      <nav className="flex gap-4">
+        <a href="#">Home</a>
+        <a href="#">About</a>
+      </nav>
+    </div>
+    <div className="flex gap-2">
+      <button>Sign In</button>
+      <button>Get Started</button>
+    </div>
+  </header>
+);
+
 }
 
 export default App
--- lepitech-frontend/src/main.tsx
+++ lepitech-frontend/src/main.tsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import App from './App'
+import { Navbar01 } from './components/ui/shadcn-io/navbar-01/index'
+import { Navbar03 } from './components/ui/shadcn-io/navbar-03/index'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <div className="min-h-screen bg-background text-foreground font-sans antialiased">
+      
+      <Navbar03 />
+    </div>
   </StrictMode>,
 )
