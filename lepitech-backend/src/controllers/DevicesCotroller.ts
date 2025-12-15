b21039ef289fb157a21b0d6594de3d9a11097136
KraetSpg <man220274@spengergasse.at>
2025-12-15 11:54:36 +0100

    fixed comparison for cpu and os

--- lepitech-backend/src/controllers/DevicesCotroller.ts
+++ lepitech-backend/src/controllers/DevicesCotroller.ts
@@ -1,16 +1,17 @@
 import { db } from "../db";
-import { aggregatedRequirements, devices, SelectDevices } from "../db/schema";
+import { aggregatedRequirementsTable, devicesTable, SelectDevices } from "../db/schema";
 import { eq, inArray } from "drizzle-orm";
 
 export class DevicesController {
 
-    public async getDevicesForAggregatedSoftware(aggregatedRequirementsId: number): Promise<SelectDevices[]> {
-        const [aggregatedRequirement] = await db.select().from(aggregatedRequirements).where(eq(aggregatedRequirements.id, aggregatedRequirementsId));
-        const allDevices : SelectDevices[] = await db.select().from(devices);
+    public async getDevicesForAggregatedSoftware(aggregatedRequirementId: number): Promise<SelectDevices[]> {
+        const [aggregatedRequirement] = await db.select().from(aggregatedRequirementsTable).where(eq(aggregatedRequirementsTable.id, aggregatedRequirementId));
+        const allDevices : SelectDevices[] = await db.select().from(devicesTable);
         const fittingDevices : SelectDevices[] = [];
 
-        // Compare Logic Funktioniert nicht für CPU
+        // Compare Logic Funktioniert nicht für CPU und OS
         allDevices.forEach((device) => {
+            // Returns if not fitting
             if (device.ram < aggregatedRequirement.ram) {
                 return;
             }
@@ -20,12 +21,13 @@ export class DevicesController {
             if (!aggregatedRequirement.os.includes(device.os)) {
                 return;
             }
-            // NOT WORKING RN
-            /*
-            if (device.cpu < aggregatedRequirement.cpu) {
-                return
+            if (device.cpu_cores && aggregatedRequirement.cpu_cores && device.cpu_cores < aggregatedRequirement.cpu_cores) {
+                return;
             }
-            */
+            if (device.cpu_frequency && aggregatedRequirement.cpu_frequency && device.cpu_frequency < aggregatedRequirement.cpu_frequency) {
+                return;
+            }
+            
             fittingDevices.push(device);
         })
 
